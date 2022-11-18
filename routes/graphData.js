1b0aae9ebb77ed49920a6e3363ac2f5c9d0ae7ac
moeez <moeezk115@gmail.com>
2022-11-18 14:38:48 +0200

    v3

--- routes/graphData.js
+++ routes/graphData.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const graphData = require("../models/v1_data_model");
+const graphDataV3 = require("../models/v3_data_model");
 
 // GET method to retrieve data by set_id
 // Returns Array that contains arrays of objects.
@@ -12,7 +13,13 @@ router.get("/v1", async (req, res, next) => {
   }
 });
 
-
+router.get("/v3", async (req, res, next) => {
+  try {
+    res.status(200).json(await graphDataV3.getV3Data(req.params.id));
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST method to insert all datasets required for V1 into the database
 // 6 datasets = 6 set_ids
@@ -20,6 +27,14 @@ router.get("/v1", async (req, res, next) => {
 router.post("/v1", async (req, res, next) => {
   try {
     res.status(200).json(await graphData.setV1());
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/v3", async (req, res, next) => {
+  try {
+    res.status(200).json(await graphDataV3.setV3());
   } catch (error) {
     next(error);
   }
