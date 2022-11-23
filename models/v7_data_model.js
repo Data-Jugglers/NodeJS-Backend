47989c94eab973de9eb391e1ca7de76824eb18d9
pochapali <ivantrufan27@gmail.com>
2022-11-23 13:24:22 +0200

    I changed V7 scripts and I made working POST request for V7 data

--- models/v7_data_model.js
+++ models/v7_data_model.js
@@ -7,20 +7,19 @@ const V7Data3 = require("../files_output/V7/V7_3.json");
 const V7Data4 = require("../files_output/V7/V7_4.json");
 
 const V7Desc = require("../files_output/V7/V7Desc.json");
-const SET_ID = 17;
-
+const SET_ID = [17, 18, 19, 20];
 const getV7Data = async () => {
   let allResults = [];
 
   const result = await db.query(
     "select * from datasets where set_id=$1 order by data_id asc",
-    [SET_ID]
+    [SET_ID[0]]
   );
   allResults.push(result.rows);
 
   const description = await db.query(
     "select * from description where set_id=$1",
-    [SET_ID]
+    [SET_ID[0]]
   );
   allResults.push(description.rows);
 
@@ -28,36 +27,47 @@ const getV7Data = async () => {
 };
 
 const setV7Components = async () => {
-  await db.query(
-    "insert into description (set_id, source_link, description_link, description) values($1,$2,$3,$4)",
-    [
-      SET_ID,
-      V7Desc.description.srcLink,
-      V7Desc.description.desLink,
-      V7Desc.description.des,
-    ]
-  );
+  for (let i = 0; i <= 3; i++) {
+    // const id = SET_ID[i];2
+    await db.query(
+      "insert into description (set_id, source_link, description_link, description) values($1,$2,$3,$4)",
+      [
+        SET_ID[i],
+        V7Desc.description.srcLink,
+        V7Desc.description.desLink,
+        V7Desc.description.des,
+      ]
+    );
+  }
   for (const element of V7Data1.data) {
     await db.query(
       "insert into datasets (set_id, measurement_date, data) values ($1,$2,$3)",
-      [SET_ID, element["Time (kyr BP)"], element["0.975"]]
+      [17, element["Time"], element["0.975"]]
     );
   }
-  //   for (const element of V7Data2.data) {
-  //     await db.query(
-  //       "insert into datasets (set_id, measurement_date, data) values ($1,$2,$3)",
-  //       [
-  //         SET_ID + 1,
-  //         element["Time (kyr BP)"],
-  //         element["Antarctic temperature change (\u00baC)"],
-  //       ]
-  //     );
-  //   }
+  for (const element of V7Data2.data) {
+    await db.query(
+      "insert into datasets (set_id, measurement_date, data) values ($1,$2,$3)",
+      [18, element["Time"], element["Data"]]
+    );
+  }
+  for (const element of V7Data3.data) {
+    await db.query(
+      "insert into datasets (set_id, measurement_date, data) values ($1,$2,$3)",
+      [19, element["Time"], element["Data"]]
+    );
+  }
+  for (const element of V7Data4.data) {
+    await db.query(
+      "insert into datasets (set_id, measurement_date, data) values ($1,$2,$3)",
+      [SET_ID[3], element["Time"], element["Data"]]
+    );
+  }
 };
 
 const setV7 = async () => {
   const check = await db.query("select * from datasets where set_id=$1", [
-    SET_ID,
+    SET_ID[0],
   ]);
   if (check.rows[0]) return "V7 is already set";
   return setV7Components();
--- scripts/V7.py
+++ scripts/V7.py
@@ -11,12 +11,29 @@ for i in [0, 1, 2, 3]:
     json_str = excel_data_df.to_dict(orient='records')
     json_dict = dict()
     json_dict["data"] = json_str
+    # if ("Antarctic temperature change (\u00baC)" in json_dict["data"][0].keys()):
+    #     json_dict["data"][0]["AData"] = json_dict["data"][
+    #         0]["Antarctic temperature change (\u00baC)"]
+    #     del json_dict["data"][0]["Antarctic temperature change (\u00baC)"]
     if (i == 0):
         for data_entry in json_dict["data"]:
 
             data_entry["Time (kyr BP)"] = "0" * \
                 (4-len(str(data_entry["Time (kyr BP)"]))) + \
                 str(data_entry["Time (kyr BP)"])
+
+    def replaceKey(initialKey, wantedKey, data):
+        if (initialKey in data.keys()):
+            data[wantedKey] = data[initialKey]
+            del data[initialKey]
+    for data_entry in json_dict["data"]:
+        replaceKey("Time (kyr BP)", "Time", data_entry)
+        replaceKey("Time (yr BP)", "Time", data_entry)
+        replaceKey("Antarctic temperature change (\u00baC)",
+                   "Data", data_entry)
+        replaceKey("Carbon dioxide (ppm)", "Data", data_entry)
+        replaceKey("d18O", "Data", data_entry)
+
     # print('Excel Sheet to JSON:\n', json_str)
     with open("files_output/V7/V7_"+str(i+1)+".json", 'w', encoding='utf-8') as jsonf:
         # jsonf.write(json_dict)
