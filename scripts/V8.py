bd468d91f0b7a3d3873bb66375b70f2c69fb1591
pochapali <ivantrufan27@gmail.com>
2022-11-28 15:47:46 +0200

    Now it changes NaNs to zero, again...

--- scripts/V8.py
+++ scripts/V8.py
@@ -7,17 +7,15 @@ url = 'https://data.icos-cp.eu/licence_accept?ids=%5B%22lApekzcmd4DRC34oGXQqOxbJ
 r = requests.get(url)
 
 excel_data_df = pandas.read_excel(
-    r.content, sheet_name="Territorial Emissions", skiprows=11).replace(np.nan, 0, regex=True)
-print(excel_data_df)
+    r.content, sheet_name="Territorial Emissions", skiprows=11
+).replace(np.nan, 0, regex=True)  # uncomment replace() to replace NaNs with zeroes
+
+# print(excel_data_df)
 json_str = excel_data_df.to_dict(
     orient='records')
 
 json_dict = dict()
 json_dict["data"] = json_str
-# if ("Antarctic temperature change (\u00baC)" in json_dict["data"][0].keys()):
-#     json_dict["data"][0]["AData"] = json_dict["data"][
-#         0]["Antarctic temperature change (\u00baC)"]
-#     del json_dict["data"][0]["Antarctic temperature change (\u00baC)"]
 
 
 def replaceKey(initialKey, wantedKey, data):
@@ -29,7 +27,15 @@ def replaceKey(initialKey, wantedKey, data):
 for data_entry in json_dict["data"]:
     replaceKey("Unnamed: 0", "Time", data_entry)
 
-# print('Excel Sheet to JSON:\n', json_str)
+   # uncomment the section below to delete NaN entries
+
+    # NaNKeys = []  # list of keys whose data is NaN
+    # for i in data_entry.keys():
+    #     if (np.isnan(data_entry[i])):
+    #         NaNKeys.append(i)
+    # for i in NaNKeys:
+    #     del data_entry[i]
+
 with open("files_output/V8/V8.json", 'w', encoding='utf-8') as jsonf:
     # jsonf.write(json_dict)
     json.dump(json_dict, jsonf, indent=4)
