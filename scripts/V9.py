254fff4dadd9618d184354c0166fc6d12b1c6032
pochapali <ivantrufan27@gmail.com>
2022-12-01 20:38:15 +0200

    For some funny reason before my previous commit my code worked as expected, just right after commit it didn't work as expected, I don't understand why it worked, because later I found the issue and in this commit I fixed the issue

--- scripts/V9.py
+++ scripts/V9.py
@@ -20,17 +20,17 @@ for i in ["Sector", "Sub-sector", "Sub-sector (further breakdown)"]:
         json_dict["Transport"] = json_str[0:5]
         json_dict["Energy in buildings (elec and heat)"] = json_str[5:7]
         json_dict["Energy in industry"] = json_str[7:14]
-        json_dict["Energy in Agri & Fishing"] = json_str[14]
-        json_dict["Unallocated fuel combustion"] = json_str[15]
+        json_dict["Energy in Agri & Fishing"] = [json_str[14]]
+        json_dict["Unallocated fuel combustion"] = [json_str[15]]
         json_dict["Fugitive emissions from energy"] = json_str[16:18]
-        json_dict["Cement"] = json_str[18]
-        json_dict["Chemical & petrochemical (industrial)"] = json_str[19]
-        json_dict["Livestock & Manure"] = json_str[20]
-        json_dict["Rice Cultivation"] = json_str[21]
-        json_dict["Agricultural Soils"] = json_str[22]
+        json_dict["Cement"] = [json_str[18]]
+        json_dict["Chemical & petrochemical (industrial)"] = [json_str[19]]
+        json_dict["Livestock & Manure"] = [json_str[20]]
+        json_dict["Rice Cultivation"] = [json_str[21]]
+        json_dict["Agricultural Soils"] = [json_str[22]]
         json_dict["Crop Burning"], json_dict["Forest Land"], json_dict["Cropland"], \
-            json_dict["Grassland"], json_dict["Landfills"], json_dict["Wastewater"] = json_str[
-                23], json_str[24], json_str[25], json_str[26], json_str[27], json_str[28]
+            json_dict["Grassland"], json_dict["Landfills"], json_dict["Wastewater"] = [json_str[
+                23]], [json_str[24]], [json_str[25]], [json_str[26]], [json_str[27]], [json_str[28]]
     if (i == "Sub-sector"):
         json_dict["Energy"] = json_str[0:6]
         json_dict["Industrial processes"] = json_str[6:8]
