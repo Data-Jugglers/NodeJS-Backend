8840ec3a64680cc2f3a70e1a81e9d112528b0837
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-12-10 18:43:47 +0200

    Fixed data insertion bugs

--- models/v10_data_model.js
+++ models/v10_data_model.js
@@ -11,7 +11,7 @@ const getV10Data = async () => {
   for (let i = 0; i < SET_ID.length; i++) {
     const ID = SET_ID[i];
     const result = await db.query(
-      "select * from datasets where set_id=$1 order by measurement_date asc",
+      "select * from datasets where set_id=$1 order by measurement_date::int asc",
       [ID]
     );
     allResults.push(result.rows);
@@ -43,15 +43,15 @@ const setV10Components = async () => {
       for (let j = 0; j < V10V4Data.data.length; j++) {
         const element = V10V4Data.data[j];
         await db.query(
-          "insert into datasets (set_id, measurement_date, data, string) values ($1,$2,$3)",
-          [ID, element.Time, 0, element.Event]
+          "insert into datasets (set_id, measurement_date, data, string) values ($1,$2,$3,$4)",
+          [ID, element.Year, 0, element.Event]
         );
       }
     } else {
       for (let j = 0; j < V10V7Data.data.length; j++) {
         const element = V10V7Data.data[j];
         await db.query(
-          "insert into datasets (set_id, measurement_date, data, string) values ($1,$2,$3)",
+          "insert into datasets (set_id, measurement_date, data, string) values ($1,$2,$3,$4)",
           [ID, element.YearsBP, 0, element.Event]
         );
       }
